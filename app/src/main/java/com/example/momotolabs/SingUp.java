05a9b7f54c581456b4b613794b14d110a8b56193
Matheo-ceron15 <mateovasques6@gmail.com>
2021-09-11 14:23:02 -0600

    primer commit
    Creacion del proyecto en android studio, creacion de diseños de la primer y segunda plantilla

---
+++ app/src/main/java/com/example/momotolabs/SingUp.java
@@ -0,0 +1,25 @@
+package com.example.momotolabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class SingUp extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up);
+
+    }
+
+    public void regresar(View view) {
+
+        Intent intent = new Intent(SingUp.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
