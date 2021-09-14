5e9f0070a6af545deb1bf511bff89d6b1b5c1668
Matheo-ceron15 <mateovasques6@gmail.com>
2021-09-14 14:32:41 -0600

    Añadicion de ultimos retoques del diseño

--- app/src/main/java/com/example/momotolabs/MainActivity.java
+++ app/src/main/java/com/example/momotolabs/MainActivity.java
@@ -1,12 +1,17 @@
     package com.example.momotolabs;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
     public class MainActivity extends Activity {
         private EditText mail, pass;
