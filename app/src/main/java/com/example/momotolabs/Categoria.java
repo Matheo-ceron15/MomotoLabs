940c6c0fc6426f49c7aa96f347685a44365a0636
Matheo-ceron15 <mateovasques6@gmail.com>
2021-09-14 12:07:04 -0600

    Correccion de diseño

--- app/src/main/java/com/example/momotolabs/Categoria.java
+++ app/src/main/java/com/example/momotolabs/Categoria.java
@@ -3,7 +3,9 @@ package com.example.momotolabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Categoria extends Activity {
 
@@ -12,5 +14,19 @@ public class Categoria extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categoria);
 
+    }
+
+    public void programing(View view) {
+
+    }
+
+    public void desing(View view) {
+
+    }
+
+    public void regresar(View view) {
+
+        Intent intent = new Intent(Categoria.this, MainActivity.class);
+        startActivity(intent);
     }
 }
